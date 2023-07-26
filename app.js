95a6e9f44200c6a10f7496af3dfd36970b19aef4
ElviraDzh <elvira.jumalieva@gmail.com>
2023-07-26 17:19:19 +0100

    Added sidebar.

--- app.js
+++ app.js
@@ -5,31 +5,30 @@ function moveShape(x, y) {
   const square = document.getElementById("square");
   if (square) {
     let transformAttr = " translate(" + x + "," + y + ")";
-    console.log(transformAttr);
     square.setAttribute("transform", transformAttr);
   }
 }
-moveShape(40, 40);
+moveShape("square", 0, 0);
 
 document.addEventListener("keydown", (e) => {
   switch (e.keyCode) {
     case 37:
-      moveShape(currentX - 40, currentY);
+      moveShape("square", currentX - 40, currentY);
       console.log("left:" + currentY);
       currentX -= 40;
       break;
     case 38:
-      moveShape(currentX, currentY - 40);
+      moveShape("square", currentX, currentY - 40);
       currentY -= 40;
       console.log("up:" + currentY);
       break;
     case 39:
-      moveShape(currentX + 40, currentY);
+      moveShape("square", currentX + 40, currentY);
       currentX += 40;
       console.log("right:" + currentX);
       break;
     case 40:
-      moveShape(currentX, currentY + 40);
+      moveShape("square", currentX, currentY + 40);
       currentY += 40;
       console.log("down:" + currentY);
       break;
